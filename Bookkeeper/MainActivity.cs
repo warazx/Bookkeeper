977f511f6582b06e00950362d9ebc0219c4d06b7
Christian Karlsson <krille.karlsson@gmail.com>
2017-01-25 12:08:30 +0100

    added button click to start new activity for new entries

--- Bookkeeper/MainActivity.cs
+++ Bookkeeper/MainActivity.cs
@@ -1,16 +1,28 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using Android.Content;
 
 namespace Bookkeeper
 {
     [Activity(Label = "Bookkeeper", MainLauncher = true, Icon = "@drawable/icon")]
     public class MainActivity : Activity
     {
+        Button newEntryBtn;
+
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
-            SetContentView (Resource.Layout.Main);
+            SetContentView(Resource.Layout.Main);
+            newEntryBtn = FindViewById<Button>(Resource.Id.newEntryBtn);
+
+            newEntryBtn.Click += NewEntryBtn_Click;
+        }
+
+        private void NewEntryBtn_Click(object sender, System.EventArgs e)
+        {
+            Intent intent = new Intent(this, typeof(NewEntryActivity));
+            StartActivity(intent);
         }
     }
 }
--- Bookkeeper/Resources/Resource.Designer.cs
+++ Bookkeeper/Resources/Resource.Designer.cs
@@ -85,6 +85,9 @@ namespace Bookkeeper
 			// aapt resource value: 0x7f030000
 			public const int Main = 2130903040;
 			
+			// aapt resource value: 0x7f030001
+			public const int NewEntry = 2130903041;
+			
 			static Layout()
 			{
 				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
