4fb4de0cba9aec26d91acc27d70edcc11e1c2b6b
Christian Karlsson <krille.karlsson@gmail.com>
2017-01-25 14:09:49 +0100

    bound all layouts to variables in the activity

--- Bookkeeper/NewEntryActivity.cs
+++ Bookkeeper/NewEntryActivity.cs
@@ -17,12 +17,35 @@ namespace Bookkeeper
     public class NewEntryActivity : Activity
     {
         BookkeeperManager bm;
+        RadioButton incomeRBtn;
+        RadioButton expenseRBtn;
+        EditText dateText;
+        EditText descriptionText;
+        Spinner typeSpin;
+        Spinner accountSpin;
+        EditText totalText;
+        EditText taxText;
+        Button addBtn;
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             SetContentView(Resource.Layout.NewEntry);
+            bindLayout();
+        }
+
+        private void bindLayout()
+        {
             bm = new BookkeeperManager();
+            incomeRBtn = FindViewById<RadioButton>(Resource.Id.newEntryRBtnIncome);
+            expenseRBtn = FindViewById<RadioButton>(Resource.Id.newEntryRBtnExpense);
+            dateText = FindViewById<EditText>(Resource.Id.newEntryDate);
+            descriptionText = FindViewById<EditText>(Resource.Id.NewEntryDescription);
+            typeSpin = FindViewById<Spinner>(Resource.Id.NewEntrySpinType);
+            accountSpin = FindViewById<Spinner>(Resource.Id.NewEntrySpinAccount);
+            totalText = FindViewById<EditText>(Resource.Id.NewEntryTotal);
+            taxText = FindViewById<EditText>(Resource.Id.NewEntryTax);
+            addBtn = FindViewById<Button>(Resource.Id.newEntryAddEntryBtn);
         }
     }
 }
